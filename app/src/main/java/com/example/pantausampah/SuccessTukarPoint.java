380e5568277a4d882be4c9e237ccd14c565b8467
Ijat <turamadhanizza@gmail.com>
2024-01-16 10:17:05 +0700

    tambah halaman berhasil tukar poin

--- app/src/main/java/com/example/pantausampah/SuccessTukarPoint.java
+++ app/src/main/java/com/example/pantausampah/SuccessTukarPoint.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class SuccessTukarPoint extends AppCompatActivity {
 
     Button btnKembali;
+    ImageView btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +19,21 @@ public class SuccessTukarPoint extends AppCompatActivity {
         setContentView(R.layout.activity_success_tukar_point);
 
         btnKembali = findViewById(R.id.kembali);
+        btnBack = findViewById(R.id.btnBack);
 
         btnKembali.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), TukarPoint.class);
                 startActivity(intent);
                 finish();
             }
